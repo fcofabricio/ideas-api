8893ca52f9e6a48402b85c39ea730815d1ec38cb
Fabrício Lima <fabricio.lima@me.com>
2022-07-13 13:50:52 -0300

    Expose Doorkeeper user to JR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
-class ApplicationController < ActionController::API
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
+  skip_before_action :verify_authenticity_token
+
+  private def context
+    {current_user:}
+  end
+
+  private def current_user
+    @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
